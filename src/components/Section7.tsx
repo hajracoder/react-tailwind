671a4e6cde9c12f7f6951544c16f6c325888f272
hajracoder <hajraqadir94@gmail.com>
2025-03-23 19:03:05 -0600

    Update Section7.tsx

--- src/components/Section7.tsx
+++ src/components/Section7.tsx
@@ -2,7 +2,7 @@ import bgImage from "../assets/10.jpeg"; // Apni image ka path lagao
 
 export default function HeroSection() {
   return (
-    <section className="relative w-full h-[85vh] md:h-screen">
+    <section className="relative w-full h-[65vh] md:h-screen">
       {/* Background Image */}
       <img
         src={bgImage}
@@ -15,20 +15,16 @@ export default function HeroSection() {
 
       {/* Content */}
       <div className="absolute inset-0 flex flex-col justify-center px-6 md:px-20 text-white">
-        <div className="max-w-md 
-          text-center md:text-center lg:text-left   /* Tab & Mobile Center, Desktop Left */
-          mx-auto lg:mx-0">  {/* Mobile & Tab Center, Desktop Left */}
-
-          <h2 className="text-3xl md:text-5xl font-semibold">Get Yours Today!</h2>
-          <p className="mt-2 text-base md:text-lg">
+        <div className="max-w-md text-center md:text-center lg:text-left mx-auto lg:mx-0">
+          <h2 className="text-2xl md:text-5xl font-semibold">Get Yours Today!</h2>
+          <p className="mt-1 text-sm md:text-lg">
             Don't wait to experience the best in oral care! Click 'Buy now' to secure your favorites and enjoy a brighter, healthier smile.
           </p>
-          
+
           {/* Button Positioning */}
-          <button className="mt-40 md:mt-6 px-6 py-3 bg-white text-black text-lg font-medium hover:bg-gray-200 transition">
+          <button className="mt-8 md:mt-6 px-6 py-3 bg-white text-black text-lg font-medium hover:bg-gray-200 transition">
             Buy now with 15% off
           </button>
-
         </div>
       </div>
     </section>
